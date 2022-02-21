8186ae0a6d5125bb3fc51538718746a562b7b208
sawhney17 <80150109+sawhney17@users.noreply.github.com>
2022-02-21 20:14:12 +0400

    fix issues for next beta

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App: React.FC<{htmlText}> = ({htmlText}) => {
 
   function handleInputChange(event) {
     const target = event.target;
-    console.log(target.value)
+    console.log(target.checked)
     // const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
   };
@@ -13,9 +13,10 @@ const App: React.FC<{htmlText}> = ({htmlText}) => {
   return (
     <div>
     <div className="w-screen h-screen flex items-center justify-center text-black">
-      
+    
       <div className="w-5/6 h-5/6 bg-clip-padding">
       <div className="bg-slate-400 z-30 rounded-2xl p-4">
+      
         <h1 className="font-bold text-4xl">PDF Preview</h1>
         <br></br>
         <div dangerouslySetInnerHTML={{__html: htmlText}} id = "cooldiv" className='bg-white rounded-xl'></div> 
@@ -23,17 +24,12 @@ const App: React.FC<{htmlText}> = ({htmlText}) => {
         <div className=''><button className='button' onClick={() => downloadPDF()}>Download</button></div>
 
         {/* <div><label><input type="checkbox"/>Value</label></div> */}
-        <div><label><input type="checkbox" onChange={ e => handleInputChange(e)}/>hide page properties</label></div>
+        <div><label><input type="checkbox" name = "Page Properties" onChange={ e => handleInputChange(e)}/>hide page properties</label></div>
       <div><label><input type="checkbox"/>hide brackets</label></div>
       <div><label><input type="checkbox"/>render with bullets</label></div>
       <div><label><input type="checkbox"/>My Value</label></div>
       </div>
-      
       </div>
-      
-      
-      
-
     </div>
     <div className="w-screen h-screen top-0 left-0 z-50" onClick={() => logseq.hideMainUI()}></div>
     </div>
