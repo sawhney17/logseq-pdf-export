d2939deef1c4bdb88e23d96288d4fc2bec4ce6e4
sawhney17 <80150109+sawhney17@users.noreply.github.com>
2022-03-18 09:07:16 +0400

    add support for templates

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 import { useState } from 'react'
 import './App.css'
 import { downloadPDF } from './main'
+var hello = "hello"
 const App: React.FC<{htmlText}> = ({htmlText}) => {
 
+  
   function handleInputChange(event) {
     const target = event.target;
     console.log(target.checked)
     // const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
   };
-
+//   return (
+//     <div>
+//     <div className='flex justify-center h-screen w-screen'>
+//    <div className=' bg-slate-400 settings-popup centered-element rounded-xl' >
+//      <div className='p-4'>
+//      <h1 className='text-xl text'>Choose your template!</h1>
+//      <br></br>
+// <div className='form-group'>
+// <button className='button' value="template1">Template 1</button>
+// <button className='button' value="template2">Template 2</button>
+// <button className='button' value="template3">Template 3</button>
+// </div>
+//    </div> 
+//    </div>
+//    </div>
+//    </div>
+//   )
   return (
     <div>
     <div className="w-screen h-screen flex items-center justify-center text-black">
@@ -24,10 +42,10 @@ const App: React.FC<{htmlText}> = ({htmlText}) => {
         <div className=''><button className='button' onClick={() => downloadPDF()}>Download</button></div>
 
         {/* <div><label><input type="checkbox"/>Value</label></div> */}
-        <div><label><input type="checkbox" name = "Page Properties" onChange={ e => handleInputChange(e)}/>hide page properties</label></div>
+        {/* <div><label><input type="checkbox" name = "Page Properties" onChange={ e => handleInputChange(e)}/>hide page properties</label></div>
       <div><label><input type="checkbox"/>hide brackets</label></div>
       <div><label><input type="checkbox"/>render with bullets</label></div>
-      <div><label><input type="checkbox"/>My Value</label></div>
+      <div><label><input type="checkbox"/>My Value</label></div> */}
       </div>
       </div>
     </div>
@@ -37,22 +55,3 @@ const App: React.FC<{htmlText}> = ({htmlText}) => {
 }
 
 export default App
-//<div className="w-screen h-screen flex items-center justify-center text-black">
-      
-//      <div className="w-5/6 h-5/6 bg-clip-padding bg-slate-400 z-30 rounded-2xl p-4">
-  //      <h1 className="font-bold text-4xl">PDF Preview</h1>
-    //    <br></br>
-      //  <div dangerouslySetInnerHTML={{__html: htmlText}} id = "cooldiv" className='bg-white rounded-xl'></div> 
-        //<br></br>
-//        <div className=''><button className='button' onClick={() => downloadPDF()}>Download</button></div>
- //       {/* <div><label><input type="checkbox"/>Value</label></div> */}
-   //     <div><label><input type="checkbox"/>hide page properties</label></div>
-     // <div><label><input type="checkbox"/>hide brackets</label></div>
-      //<div><label><input type="checkbox"/>render with bullets</label></div>
-      //<div><label><input type="checkbox"/>My Value</label></div>
-     // </div>
-      
-      //<div className="w-screen h-screen fixed top-0 left-0 z-10" onClick={() => logseq.hideMainUI()}></div>
-      
-
-    //</div>
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import '@logseq/libs'
 import React from 'react'
 import ReactDOM, { render } from 'react-dom'
 import App from './App'
+import App2 from './App2'
 import './index.css'
 import { handleClosePopup } from './handleClosePopup';
 import { jsPDF } from "jspdf";
@@ -14,15 +15,64 @@ import markdownMark from 'markdown-it-mark'
 function encodeImage(){
 }
 
-let settings:SettingSchemaDesc = 
-  {
-    key: "css",
+// function ApplyUserSettings(){
+//   let settings = logseq.settings[]
+//   if logseq.settings.
+//   if logseq.settings.
+//   if logseq.settings.
+// }
+const propertyOptions = ["Hide Page Properties", "Hide Brackets", "Include Bullets?"]
+let settings:SettingSchemaDesc[] = 
+  [{
+    key: "template1CSS",
     type: 'string',
     default: "",
     title: "Template 1 Css",
     description: "Enter the css for the template 1",
-  }
-logseq.useSettingsSchema([settings])
+  },
+  {
+    key: "template1Options",
+    type: 'enum',
+    default: "",
+    title: "Template 1 Options",
+    enumChoices: propertyOptions,
+    enumPicker: 'checkbox',
+    description: "Pick Options for Template 1"
+  },
+  {
+    key: "template2CSS",
+    type: 'string',
+    default: "",
+    title: "Template 2 Css",
+    description: "Enter the css for the template 2",
+  },
+  {
+    key: "template2Options",
+    type: 'enum',
+    default: "",
+    title: "Template 2 Options",
+    enumChoices: propertyOptions,
+    enumPicker: 'checkbox',
+    description: "Pick Options for Template 2"
+  },
+  {
+    key: "template3CSS",
+    type: 'string',
+    default: "",
+    title: "Template 3 Css",
+    description: "Enter the css for the template 3",
+  },
+  {
+    key: "template3Options",
+    type: 'enum',
+    default: "",
+    title: "Template 3 Options",
+    enumChoices: propertyOptions,
+    enumPicker: 'checkbox',
+    description: "Pick Options for Template 3"
+  },
+]
+logseq.useSettingsSchema(settings)
 export function downloadPDF(){
   const pdf = new jsPDF({
     orientation: 'p',
@@ -33,7 +83,7 @@ export function downloadPDF(){
   // var getContent = "<div style='font-size:11px; border:1px solid; background-color: rgb(239 240 240); padding: 05px 15px; width:300px;'>"+document.body+"</div>";
 
   pdf.html(document.getElementById("you are cool"), {
-        callback: (doc) => doc.save('filename342.pdf'),
+        callback: (doc) => doc.save('logseqDownloaded.pdf'),
         margin: 0,
         width: 595,
         windowWidth: 595,
@@ -57,7 +107,8 @@ import markdownIt from 'markdown-it'
 import { SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin.user'
 
 async function formatText(text2){
-  var text = text2.replace(/:LOGBOOK:|collapsed:: true/gi, '');
+  
+  var text:string = text2.replace(/:LOGBOOK:|collapsed:: true/gi, '');
       if (text.includes('CLOCK: [')) {
         text = text.substring(0, text.indexOf('CLOCK: ['));
       }
@@ -68,6 +119,7 @@ async function formatText(text2){
             const block = await logseq.Editor.getBlock(blockId[1], {
               includeChildren: true,
             });
+        //optional based on setting enabled 
 console.log(blockId)
 console.log(block)
 if (block!= null ){
@@ -90,7 +142,7 @@ var md = new markdownIt().use(markdownMark)
 // md.inline.ruler.enable([
 //   'mark'
 // ]);
-async function createPDF () {
+export async function createPDF (templateName) {
     const currentBlock = await logseq.Editor.getCurrentPageBlocksTree()
     console.log(currentBlock)
     for (const x in currentBlock){
@@ -105,19 +157,34 @@ async function createPDF () {
         formattedText = "  " + formattedText
       }
       
+    //Filter to remove bullets when they are hastags as well
       finalString = `${finalString}\n\n ${formattedText}`
+console.log
     }
 
+    //Perform removal functions
+    finalString = finalString.replaceAll("#+BEGIN_QUOTE", "")
+    finalString = finalString.replaceAll("#+END_QUOTE", "")
+    //implement user settings based removal
+    // if (logseq.settings[`${templateName}Options`].includes("Hide Page Properties")){
+      console.log(logseq.settings[`${templateName}Options`])
+
+    // finalString = finalString.replaceAll(/- (#)\1{0,}./g,replacerFunctoin)
+    function replacerFunctoin(match){
+      console.log(match)
+    return match.substring(2)
+    }
+    
     const baseCSS = "h1 {     display: block;     font-size: 2em;     margin-block-start: 0.67__qem;     margin-block-end: 0.67em;     margin-inline-start: 0;     margin-inline-end: 0;     font-weight: bold; } :is(article, aside, nav, section) h1 {     font-size: 1.5em;     margin-block-start: 0.83__qem;     margin-block-end: 0.83em; } :is(article, aside, nav, section) :is(article, aside, nav, section) h1 {     font-size: 1.17em;     margin-block-start: 1__qem;     margin-block-end: 1em; } :is(article, aside, nav, section) :is(article, aside, nav, section) :is(article, aside, nav, section) h1 {     font-size: 1.00em;     margin-block-start: 1.33__qem;     margin-block-end: 1.33em; } :is(article, aside, nav, section) :is(article, aside, nav, section) :is(article, aside, nav, section) :is(article, aside, nav, section) h1 {     font-size: .83em;     margin-block-start: 1.67__qem;     margin-block-end: 1.67em; } :is(article, aside, nav, section) :is(article, aside, nav, section) :is(article, aside, nav, section) :is(article, aside, nav, section) :is(article, aside, nav, section) h1 {     font-size: .67em;     margin-block-start: 2.33__qem;     margin-block-end: 2.33em; } h2 {     display: block;     font-size: 1.5em;     margin-block-start: 0.83__qem;     margin-block-end: 0.83em;     margin-inline-start: 0;     margin-inline-end: 0;     font-weight: bold; } h3 {     display: block;     font-size: 1.17em;     margin-block-start: 1__qem;     margin-block-end: 1em;     margin-inline-start: 0;     margin-inline-end: 0;     font-weight: bold; } h4 {     display: block;     margin-block-start: 1.33__qem;     margin-block-end: 1.33em;     margin-inline-start: 0;     margin-inline-end: 0;     font-weight: bold; } h5 {     display: block;     font-size: .83em;     margin-block-start: 1.67__qem;     margin-block-end: 1.67em;     margin-inline-start: 0;     margin-inline-end: 0;     font-weight: bold; } h6 {     display: block;     font-size: .67em;     margin-block-start: 2.33__qem;     margin-block-end: 2.33em;     margin-inline-start: 0;     margin-inline-end: 0;     font-weight: bold; } /* lists */ ul, menu, dir {     display: block;     list-style-type: disc;     margin-block-start: 1__qem;     margin-block-end: 1em;     margin-inline-start: 0;     margin-inline-end: 0;     padding-inline-start: 40px; } ol {     display: block;     list-style-type: decimal;     margin-block-start: 1__qem;     margin-block-end: 1em;     margin-inline-start: 0;     margin-inline-end: 0;     padding-inline-start: 40px; } li {     display: list-item;     text-align: match-parent; } /* FIXME: this should also match ::before::marker and ::after::marker but we don't support    this yet. When we do, we can remove the code specific to ::before and ::after in    RenderListItem::computeMarkerStyle(), see bugs.webkit.org/b/218897. */ ::marker {     unicode-bidi: isolate;     font-variant-numeric: tabular-nums;     white-space: pre;     text-transform: none; } ul ul, ol ul {     list-style-type: disc; } ol ol ul, ol ul ul, ul ol ul, ul ul ul {     list-style-type: disc; } dd {     display: block;     margin-inline-start: 40px; } dl {     display: block;     margin-block-start: 1__qem;     margin-block-end: 1em;     margin-inline-start: 0;     margin-inline-end: 0; } dt {     display: block; } ol ul, ul ol, ul ul, ol ol {     margin-block-start: 0;     margin-block-end: 0; }"
     const settings = logseq.settings
-    const css3 = settings.css
-    // const css3 = `<style>
-    // h2 {
-    //     font-size: 10px;
-    // }
-    // </style>`
-    const final2String = md.render(finalString)
+    console.log(templateName+"CSS")
+    console.log(settings[templateName+"CSS"])
+    const css3 = settings[templateName+"CSS"]
+
+    console.log(finalString)
+     var final2String = md.render(finalString)
     console.log(final2String)
+    final2String = final2String.replace()
     final3String = (`<html><head><style>${baseCSS}</style></head><body><style>${css3}</style><div id = "you are cool" style='padding: 1rem'">${final2String}</div></body></html>`)
     // let final2String = "<h1>Hello World</h1>\n\n<p>This is a paragraph</p>"
     logseq.App.getCurrentGraph().then(async (graph) => {
@@ -151,7 +218,6 @@ console.log(blocks2)
 const main = async () => {
   console.log(await logseq.App.getCurrentGraph())
 
-
   logseq.App.registerUIItem('toolbar', {
     key: 'logseq-plugin-react-boilerplate',
     template: '<a data-on-click="show" class="button"><i class="ti ti-window"></i></a>',
@@ -160,9 +226,20 @@ const main = async () => {
     show() {
       // renderApp('logseq')
       // logseq.showMainUI()
-      createPDF()
+      // createPDF()
+      renderSecondApp()
+      logseq.showMainUI()
+      handleClosePopup()
     },
   })
+  function renderSecondApp(){
+    ReactDOM.render(
+      <React.StrictMode>
+        <App2 />
+      </React.StrictMode>,
+      document.getElementById("root")
+    )
+  }
 
   console.log('Plugin Loaded');
 
